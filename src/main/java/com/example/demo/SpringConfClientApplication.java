dead9c9941bfafef093574a64060edcea6c829be
wl00254294 <wl00254294@gmail.com>
2019-02-20 11:05:43 +0800

    init

---
+++ src/main/java/com/example/demo/SpringConfClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfClientApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/TestConfigController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestConfigController {
+	@Value("${from}")
+    private String from;
+	@Value("${from2}")
+    private String from2;
+   
+	@RequestMapping("/from")
+    public String from() {
+        return from;
+    }
+	@RequestMapping("/from2")
+    public String from2() {
+        return from2;
+    }
+}
