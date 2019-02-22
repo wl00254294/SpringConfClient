b0473e84dd6f80d7468c0935be6b209011838caa
wl00254294 <wl00254294@gmail.com>
2019-02-22 12:57:54 +0800

    add connect to erueka method

--- src/main/java/com/example/demo/SpringConfClientApplication.java
+++ src/main/java/com/example/demo/SpringConfClientApplication.java
@@ -2,8 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringConfClientApplication {
 
 	public static void main(String[] args) {
